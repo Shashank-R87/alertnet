375d6318c20d4af84bf522ddef748d638a8450dc
Shashank-R87 <shashankr8724@gmail.com>
2025-08-30 13:53:09 +0530

    "#16 FIX Notification"

--- app/(app)/(tabs)/home.tsx
+++ app/(app)/(tabs)/home.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import AlertCard from "@/components/AlertCard";
 import AlertLoader from "@/components/AlertLoader";
 import ZoneMapComponent from "@/components/ZoneMapComponent";
@@ -21,23 +20,6 @@ import {
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-=======
-import AlertCard from '@/components/AlertCard';
-import AlertLoader from '@/components/AlertLoader';
-import ZoneMapComponent from '@/components/ZoneMapComponent';
-import { useAuth } from '@/context/AuthContext';
-import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
-import { ethers } from "ethers";
-import { Image } from 'expo-image';
-import { LinearGradient } from 'expo-linear-gradient';
-import * as Location from 'expo-location';
-import * as Notifications from "expo-notifications";
-import { Link } from 'expo-router';
-import { StatusBar } from "expo-status-bar";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { ActivityIndicator, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
 import contractJson from "../../../lib/ZoneAlert.json";
 
 type Zone = {
@@ -45,11 +27,7 @@ type Zone = {
   contractAddress: string;
   latitude: number;
   longitude: number;
-<<<<<<< HEAD
   radius: number;
-=======
-  radius: number
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
 };
 
 type Alert = {
@@ -236,14 +214,10 @@ function useGeofenceStatus(
   return status;
 }
 
-<<<<<<< HEAD
 function useZoneAlerts(
   enabled: boolean,
   contractAddress?: string
 ): [boolean, Alert[]] {
-=======
-function useZoneAlerts(enabled: boolean, contractAddress?: string): [boolean, Alert[]] {
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
   const [listening, setListening] = useState(false);
   const [alerts, setAlerts] = useState<Alert[]>([]);
 
@@ -255,15 +229,11 @@ function useZoneAlerts(enabled: boolean, contractAddress?: string): [boolean, Al
     }
 
     const provider = new ethers.WebSocketProvider(INFURA_WS_URL);
-<<<<<<< HEAD
     const contract = new ethers.Contract(
       contractAddress,
       contractJson.abi,
       provider
     );
-=======
-    const contract = new ethers.Contract(contractAddress, contractJson.abi, provider);
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
 
     const handler = async (
       sender: string,
@@ -275,10 +245,6 @@ function useZoneAlerts(enabled: boolean, contractAddress?: string): [boolean, Al
       wtc: string,
       event: any
     ) => {
-<<<<<<< HEAD
-=======
-
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
       const newAlert: Alert = {
         zoneNumber,
         message,
@@ -322,21 +288,16 @@ function HomeScreen() {
   const { permission, location } = useLocation();
   const { zone, refetch } = useZone(location);
   const status = useGeofenceStatus(permission, location, zone);
-<<<<<<< HEAD
   const [listening, alerts] = useZoneAlerts(
     status === "Inside",
     zone?.contractAddress
   );
-=======
-  const [listening, alerts] = useZoneAlerts(status === "Inside", zone?.contractAddress);
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
 
   // useEffect(() => {
   //   if (!location?.coords) return;
   //   refetch(location.coords.latitude, location.coords.longitude, true);
   // }, [status]);
 
-<<<<<<< HEAD
   useEffect(() => {
     (async () => {
       const { status } = await Notifications.requestPermissionsAsync();
@@ -346,13 +307,10 @@ function HomeScreen() {
     })();
   }, []);
 
-=======
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
   const tabBarHeight = useBottomTabBarHeight();
 
   return (
     <SafeAreaView className="flex-1 items-center justify-start gap-6 p-8 bg-white">
-<<<<<<< HEAD
       {/* TODO: Header */}
       <View className="w-full flex justify-between flex-row items-center">
         <View className="flex flex-row justify-end items-end gap-4">
@@ -383,24 +341,6 @@ function HomeScreen() {
                   user?.photo ??
                   "https://placehold.co/100x100/E2E8F0/4A5568?text=No+Image",
               }}
-=======
-
-      {/* TODO: Header */}
-      <View className='w-full flex justify-between flex-row items-center'>
-        <View className='flex flex-row justify-end items-end gap-4'>
-          <Image source={require('@/assets/images/main-logo.png')} style={styles.logo} />
-          <View className='flex justify-start items-start gap-1'>
-            <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-3xl font-medium'>AlertNet</Text>
-            <View className='flex flex-row justify-start items-center gap-2'>
-              <Text style={{ fontFamily: "Poppins_400Regular" }} className='text-sm'>{`Connecting drivers, securing roads.`}</Text>
-            </View>
-          </View>
-        </View>
-        <Link href={'/profile'}>
-          <View className='flex justify-center items-end flex-1 self-center'>
-            <Image
-              source={{ uri: user?.photo ?? "https://placehold.co/100x100/E2E8F0/4A5568?text=No+Image" }}
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
               style={styles.profilePhoto}
             />
           </View>
@@ -410,7 +350,6 @@ function HomeScreen() {
       <ScrollView
         style={{ flex: 1 }}
         showsVerticalScrollIndicator={false}
-<<<<<<< HEAD
         contentContainerStyle={{
           paddingBottom: tabBarHeight,
           alignItems: "center",
@@ -446,31 +385,12 @@ function HomeScreen() {
                     zone={zone}
                     enableInteraction={false}
                   />
-=======
-        contentContainerStyle={{ paddingBottom: tabBarHeight, alignItems: 'center', justifyContent: 'flex-start', gap: 20 }}
-      >
-
-        {/* TODO: Your Information */}
-        <View className='flex flex-col w-full gap-0 bg-white rounded-xl relative'>
-          <View className='self-end bg-[#F3F4F6] px-4 pt-3 pb-1 rounded-t-xl relative top-1 flex flex-row justify-start items-center gap-2'>
-            <View className={`w-2 h-2 rounded-3xl ${listening ? "bg-green-400" : "bg-red-500"}`}></View>
-            <Text style={{ fontFamily: "Poppins_400Regular" }} className='text-base'>{listening ? "Active" : "Inactive"}</Text>
-          </View>
-          <View className='flex flex-col justify-start items-start w-full bg-[#F3F4F6] rounded-l-xl rounded-br-xl p-5 gap-2'>
-            <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-2xl'>Your Information</Text>
-            <View className='flex flex-row gap-4 justify-center items-start'>
-              {zone ? (
-                <View
-                  className='w-36 flex justify-center items-center aspect-square'>
-                  <ZoneMapComponent location={location} zone={zone} enableInteraction={false} />
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
                   {/* <Image
                     source={require('@/assets/images/noun-maps.svg')}
                     style={styles.maps}
                   /> */}
                 </View>
               ) : (
-<<<<<<< HEAD
                 <View
                   style={styles.mapPlaceholder}
                   className="w-36 flex justify-center items-center aspect-square"
@@ -551,41 +471,6 @@ function HomeScreen() {
                   <TouchableOpacity activeOpacity={0.6}>
                     <Image
                       source={require("@/assets/images/noun-expand.svg")}
-=======
-                <View style={styles.mapPlaceholder} className='w-36 flex justify-center items-center aspect-square'>
-                  <ActivityIndicator size="small" color="#808080" />
-                </View>
-              )}
-              <View className='flex-1 justify-between'>
-                <View className='flex flex-col gap-1 flex-1'>
-                  <View className='flex flex-row gap-3'>
-                    <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-lg'>Lat</Text>
-                    <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-lg text-gray-500'>{fmt(location?.coords?.latitude)}</Text>
-                  </View>
-                  <View className='flex flex-row gap-3'>
-                    <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-lg'>Long</Text>
-                    <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-lg text-gray-500'>{fmt(location?.coords?.longitude)}</Text>
-                  </View>
-                  <View className='flex flex-row gap-3'>
-                    <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-lg'>Radius</Text>
-                    <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-lg text-gray-500'>{zone?.radius ? `${zone.radius} m` : "—"}</Text>
-                  </View>
-                  <View className='flex flex-row gap-3'>
-                    <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-lg'>Presence</Text>
-                    <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-lg text-gray-500'>{status}</Text>
-                  </View>
-                </View>
-                <Link href={{
-                  pathname: '/zonemap',
-                  params: {
-                    zone: JSON.stringify(zone),
-                    location: JSON.stringify(location),
-                  },
-                }} className='flex gap-3 self-end' asChild>
-                  <TouchableOpacity activeOpacity={0.6}>
-                    <Image
-                      source={require('@/assets/images/noun-expand.svg')}
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
                       style={styles.expand}
                     />
                   </TouchableOpacity>
@@ -595,7 +480,6 @@ function HomeScreen() {
           </View>
         </View>
 
-<<<<<<< HEAD
         {/* TODO: Zone Information */}
         <View className="flex flex-col justify-start items-start w-full bg-[#F3F4F6] rounded-xl p-5 gap-2">
           <Text
@@ -626,19 +510,6 @@ function HomeScreen() {
               >
                 Address
               </Text>
-=======
-
-        {/* TODO: Zone Information */}
-        <View className='flex flex-col justify-start items-start w-full bg-[#F3F4F6] rounded-xl p-5 gap-2'>
-          <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-2xl'>Zone Information</Text>
-          <View className='flex flex-col justify-start items-start gap-1'>
-            <View className='flex flex-row gap-3'>
-              <Text className='text-lg' style={{ fontFamily: "Poppins_500Medium" }}>Zone name</Text>
-              <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-lg text-gray-500'>{zone?.zoneName ?? "—"}</Text>
-            </View>
-            <View className='flex w-full flex-row gap-3'>
-              <Text className='text-lg' style={{ fontFamily: "Poppins_500Medium" }}>Address</Text>
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
               <View style={{ flex: 1 }}>
                 <Text
                   style={{
@@ -653,7 +524,6 @@ function HomeScreen() {
                 </Text>
               </View>
             </View>
-<<<<<<< HEAD
             <View className="flex flex-row justify-between w-full">
               <View className="flex flex-row gap-3">
                 <Text
@@ -692,40 +562,17 @@ function HomeScreen() {
                     <TouchableOpacity activeOpacity={0.6}>
                       <Image
                         source={require("@/assets/images/noun-link.svg")}
-=======
-            <View className='flex flex-row justify-between w-full'>
-              <View className='flex flex-row gap-3'>
-                <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-lg'>Lat</Text>
-                <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-lg text-gray-500'>{fmt(zone?.latitude)}</Text>
-              </View>
-              <View className='flex flex-row gap-3'>
-                <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-lg'>Long</Text>
-                <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-lg text-gray-500'>{fmt(zone?.longitude)}</Text>
-              </View>
-              <View className='flex flex-row gap-3'>
-                {
-                  zone?.contractAddress &&
-                  <Link href={`https://sepolia.etherscan.io/address/${zone.contractAddress}`} asChild>
-                    <TouchableOpacity activeOpacity={0.6}>
-                      <Image
-                        source={require('@/assets/images/noun-link.svg')}
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
                         style={styles.expand}
                       />
                     </TouchableOpacity>
                   </Link>
-<<<<<<< HEAD
                 )}
-=======
-                }
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
               </View>
             </View>
           </View>
         </View>
 
         {/* TODO: Posted Alerts */}
-<<<<<<< HEAD
         <View className="flex-col justify-start items-start w-full bg-[#F3F4F6] rounded-xl p-5 gap-3 flex-1">
           <Text
             style={{ fontFamily: "Poppins_500Medium" }}
@@ -737,26 +584,13 @@ function HomeScreen() {
           {alerts.map((alert, idx) => (
             <AlertCard alert={alert} key={idx} />
           ))}
-=======
-        <View className='flex-col justify-start items-start w-full bg-[#F3F4F6] rounded-xl p-5 gap-3 flex-1'>
-          <Text style={{ fontFamily: "Poppins_500Medium" }} className='text-2xl'>Alerts</Text>
-          <AlertLoader listening={listening} />
-          {
-            alerts.map((alert, idx)=><AlertCard alert={alert} key={idx}/>)
-          }
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
         </View>
       </ScrollView>
 
       <LinearGradient
         style={styles.fadeOverlay}
-<<<<<<< HEAD
         colors={["rgba(255, 255, 255, 0)", "rgba(255, 255, 255, 1)"]}
         pointerEvents={"none"}
-=======
-        colors={['rgba(255, 255, 255, 0)', 'rgba(255, 255, 255, 1)']}
-        pointerEvents={'none'}
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
       />
       <StatusBar style="dark" />
     </SafeAreaView>
@@ -768,18 +602,13 @@ const styles = StyleSheet.create({
   logo: { width: 58, height: 58 },
   expand: { width: 22, height: 22 },
   fadeOverlay: {
-<<<<<<< HEAD
     position: "absolute",
-=======
-    position: 'absolute',
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
     bottom: 0,
     left: 0,
     right: 0,
     height: 180,
   },
   mapPlaceholder: {
-<<<<<<< HEAD
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F3F4F6", // Match your card background color
@@ -790,24 +619,8 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     flexWrap: "wrap",
-=======
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F3F4F6', // Match your card background color
-    borderRadius: 12,
-  },
-  gridContainer: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
     gap: 8,
   },
 });
 
-<<<<<<< HEAD
 export default HomeScreen;
-=======
-export default HomeScreen;
->>>>>>> 5eb9be191ca928e8617a991d9ba73eb41f9de069
